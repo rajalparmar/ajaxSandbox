d6088d3bcdece3ae9ace53e73d0577eea2fa4f29
rajal_parmar <raparmar27@gmail.com>
2019-09-05 07:16:38 -0500

    adding customer and jokes section

--- ajax-sandbox.js
+++ ajax-sandbox.js
@@ -1,7 +1,4 @@
 /* XHR Object Methods and Working with Text */
-
-document.getElementById('button').addEventListener('click', loadData);
-
 function loadData() {
     // Create an XHR object 
     const xmlHttpRequest = new XMLHttpRequest();
@@ -26,3 +23,82 @@ function loadData() {
     xmlHttpRequest.send();
 
 }
+/* XHR Object Methods and Working with JSON */
+function loadCustomer() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'customer.json', true);
+    xhr.onload = function() {
+        if(this.status === 200) {
+            console.log(this.responseText);
+            const customerData = JSON.parse(this.responseText);
+            const output = `
+                <ul>
+                    <li>ID: ${customerData.id}</li>
+                    <li>Name: ${customerData.name}</li>
+                    <li>Company: ${customerData.company}</li>
+                    <li>Phone: ${customerData.phone}</li>
+                </ul>
+            `;
+            document.getElementById('customer').innerHTML = output;
+        }
+    }
+    xhr.send();
+}
+function loadCustomers() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'customers.json', true);
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const customersData = JSON.parse(this.responseText);
+
+            let output = '';
+            customersData.forEach(function(customer) {
+                output += `
+                    <ul>
+                        <li>ID: ${customer.id}</li>
+                        <li>Name: ${customer.name}</li>
+                        <li>Company: ${customer.company}</li>
+                        <li>Phone: ${customer.phone}</li>
+                    </ul>
+                `;
+            })
+            
+            document.getElementById('customers').innerHTML = output;
+        }
+    }
+    xhr.send();
+}
+function getJokes(e) {
+    const number = document.querySelector('input[type="number"]').value;
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            let output = '';
+            if(response.type === 'success') {
+                response.value.forEach(function(value) {
+                    output += `<li>${value.joke}</li>`;
+                });
+            } else {
+                output += '<li>Something went wrong!</li>';
+            }
+            document.querySelector('.jokes').innerHTML = output;
+        }
+    }
+    xhr.send();
+
+    e.preventDefault();
+}
+
+// Event Listeners
+document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+
+
+    
